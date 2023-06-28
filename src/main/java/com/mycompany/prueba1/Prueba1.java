43018c18debf5dad81f2bf0cf4d00ce1c56cc23b
vidal7226 <vidalalvarezjosefrancisco@gmail.com>
2023-06-28 11:07:50 -0500

    first commit

---
+++ src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
